025ce3e0e5a422c11db1089285d830769dd6ae3a
DenysTXJ <denyst89@gmail.com>
2022-06-29 16:51:19 -0400

    first commit folders/packages/config.properties added

---
+++ src/test/java/Runner/CukesRunner.java
@@ -0,0 +1,21 @@
+package Runner;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "html:target/cucumber-reports.html",
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+        },
+        features ="src/test/resources/FeatureFiles",
+        glue="src/test/java/StepDefinitions",
+        dryRun=false,
+        tags = "@Librarian"
+)
+public class CukesRunner {
+}
